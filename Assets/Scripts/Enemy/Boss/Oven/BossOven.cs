1c43472c91ddaec52a7e507ecd0c93666731937a
fiiction <Kai_Jin_1998@163.com>
2019-05-16 12:04:39 +0800

    Oven modified

--- Assets/Scripts/Enemy/Boss/Oven/BossOven.cs
+++ Assets/Scripts/Enemy/Boss/Oven/BossOven.cs
@@ -16,13 +16,13 @@ public class BossOven : MonoBehaviour {
         yield return new WaitForSeconds(1F);
         LCloud = GameObject.Instantiate(AtkCloud, LHolder.transform.position, Quaternion.identity);
         LCloud.GetComponent<EnemyController>().targetPos = new Vector2(-1.98F,3.97F);
-        yield return new WaitForSeconds(0.5F);
+        yield return new WaitForSeconds(0.6F);
         LCloud = null;
 
         yield return new WaitForSeconds(1.5F);
         RCloud = GameObject.Instantiate(AtkCloud, RHolder.transform.position, Quaternion.identity);
         RCloud.GetComponent<EnemyController>().targetPos = new Vector2(1.98F, 3.97F);
-        yield return new WaitForSeconds(0.5F);
+        yield return new WaitForSeconds(0.6F);
         RCloud = null;
         yield return new WaitForSeconds(1.3F);
         SetState(State.Idle);
@@ -51,7 +51,27 @@ public class BossOven : MonoBehaviour {
         anim = GetComponent<Animator>();
         AtkCloud = Resources.Load<GameObject>("Prefabs/Enemy/DarkCloud");
     }
-	
+
+    Vector3 LLastFramePos, RLastFramePos;
+    void FixedUpdate()
+    {
+
+        if (RCloud)
+        {
+            RCloud.GetComponent<Rigidbody2D>().velocity = (RHolder.transform.position - RLastFramePos) / Time.fixedDeltaTime;
+            RCloud.transform.position = RHolder.transform.position;
+            RCloud.transform.rotation = Quaternion.identity;
+        }
+        if (LCloud)
+        {
+            LCloud.GetComponent<Rigidbody2D>().velocity = (LHolder.transform.position - LLastFramePos) / Time.fixedDeltaTime;
+            LCloud.transform.position = LHolder.transform.position;
+            LCloud.transform.rotation = Quaternion.identity;
+        }
+        LLastFramePos = LHolder.transform.position;
+        RLastFramePos = RHolder.transform.position;
+    }
+
 	// Update is called once per frame
 	void Update () {
 
@@ -63,24 +83,7 @@ public class BossOven : MonoBehaviour {
                     anim.SetBool("dragCloud", true);
                 break;
             case State.DragCloud:
-                break;       
-                
-
-        }
-
-
-        if (RCloud)
-        {
-            //RCloud.GetComponent<Rigidbody2D>().velocity = RHolder.GetComponent<Rigidbody2D>().velocity;
-            RCloud.GetComponent<Rigidbody2D>().velocity = (RHolder.transform.position - RCloud.transform.position) / Time.deltaTime;
-            RCloud.transform.position = RHolder.transform.position;
-            RCloud.transform.rotation = Quaternion.identity;
-        }
-        if (LCloud)
-        {
-            LCloud.GetComponent<Rigidbody2D>().velocity = (LHolder.transform.position - LCloud.transform.position) / Time.deltaTime;
-            LCloud.transform.position = LHolder.transform.position;
-            LCloud.transform.rotation = Quaternion.identity;
+                break;    
         }
 
     }
